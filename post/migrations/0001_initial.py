32edd1fa46e4d2f623328bc746ad82a2c79cb82a
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2020-08-26 02:33:24 +0800

    run autopep8

--- post/migrations/0001_initial.py
+++ post/migrations/0001_initial.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=20, verbose_name='標題')),
                 ('content', models.CharField(max_length=200, verbose_name='內容')),
             ],
